54a643f47506d38d6ca37432feb30eec3d40beda
Narwade-Pratik <pratik1710narwade@gmail.com>
2026-08-26 16:04:15 +0530

    Help fragement updated successfully.

--- app/src/main/java/com/vinodnarwade/eduquiz/fragments/HelpFragment.java
+++ app/src/main/java/com/vinodnarwade/eduquiz/fragments/HelpFragment.java
@@ -1,24 +1,33 @@
 package com.vinodnarwade.eduquiz.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.vinodnarwade.eduquiz.R;
 
 public class HelpFragment extends Fragment {
 
+    public HelpFragment() {
+        // Required empty public constructor
+    }
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_help, container, false);
-        return view;
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(
+                R.layout.fragment_help,
+                container,
+                false
+        );
     }
 }
